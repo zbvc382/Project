ae6dbcba5e20bf419f81e3c7ec38224b0fe81334
Zilvinas Pocius <zbvc382@live.rhul.ac.uk>
2017-10-22 20:08:37 +0100

    Added logout page and redirect.

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from mysite import views
 
 urlpatterns = [
+    url(r'^$', views.login_redirect, name='login_redirect'),
     url(r'^admin/', admin.site.urls),
     url(r'^account/', include('accounts.urls'))
 ]
