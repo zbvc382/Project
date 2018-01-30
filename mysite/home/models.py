113d6d14fa21c807ed00bcb75055feee5a4e9c3f
Zilvinas Pocius <zbvc382@live.rhul.ac.uk>
2018-01-30 19:48:06 +0000

    Added requester model and function - If a new user is created and its role is set to 'Requester' then it creates a Requester model object

--- mysite/home/models.py
+++ mysite/home/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 from django.conf import settings
 from django.contrib.auth import get_user_model
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
 User = get_user_model()
 
 
@@ -18,7 +21,7 @@ class Request(models.Model):
         (SICK_LEAVE, 'Sick Leave')
     )
 
-    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True, blank=True)
     date_requested = models.DateTimeField(auto_now_add=True, null=True, blank=True)
     leave_type = models.CharField(max_length=10, choices=LEAVE_TYPE, default=HOLIDAY)
     start = models.DateField()
@@ -26,3 +29,19 @@ class Request(models.Model):
     reason = models.TextField(max_length=200)
     status = models.CharField(max_length=10, default='pending')
     attachment = models.FileField(upload_to=user_directory_path, blank=True, null=True)
+
+
+class Requester(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL)
+
+    def __str__(self):
+        return '%s' % self.user
+
+# creates a requester object if created user's role is 'Requester'
+@receiver(post_save, sender=User)
+def create_profile(sender, instance, created, **kwargs):
+    if created and instance.user_role == 'Requester':
+        Requester.objects.create(user=instance)
+
+
+post_save.connect(create_profile, sender=User)
