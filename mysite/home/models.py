b258e729639f73c66df05f9096dafc122b8a9f5c
Zilvinas Pocius <zbvc382@live.rhul.ac.uk>
2018-02-11 01:30:35 +0000

    Different icons based on file extension

--- mysite/home/models.py
+++ mysite/home/models.py
@@ -30,8 +30,8 @@ class Request(models.Model):
     end = models.DateField()
     reason = models.TextField(max_length=200)
     status = models.CharField(max_length=10, default='Pending')
-    attachment = models.FileField(upload_to=user_directory_path, blank=True)
-    comment = models.TextField(max_length=200, blank=True, null=True)
+    attachment = models.FileField(upload_to=user_directory_path, blank=True, null=True)
+    comment = models.TextField(max_length=200, blank=True, default="")
 
     def __str__(self):
         return '%s' % self.user
