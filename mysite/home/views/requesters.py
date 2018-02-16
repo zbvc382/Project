2c7481ebc633b67b622a27252a7989cf0282803d
Zilvinas Pocius <zbvc382@live.rhul.ac.uk>
2018-02-16 22:28:34 +0000

    Modified alert message

--- mysite/home/views/requesters.py
+++ mysite/home/views/requesters.py
@@ -14,14 +14,13 @@ User = get_user_model()
 
 @method_decorator([login_required, requester_required], name='dispatch')
 class RequesterHomeView(TemplateView):
-
     template_name = 'home.html'
 
     def get_context_data(self, **kwargs):
         user = self.request.user
         requester_requests = Request.objects.filter(user=self.request.user)
 
-        # Might need exception handling later on
+        # TODO: Might need exception handling later on
         assigned_authoriser = Requester.objects.get(user=self.request.user).assigned_authoriser.user.get_full_name
         array = ['pdf', 'jpg', 'txt', 'docx']
 
@@ -41,6 +40,6 @@ class RequestView(CreateView):
         o = form.save(commit=False)
         o.user = self.request.user
         o.save()
-        messages.success(self.request, 'Form submitted successfully!')
+        messages.success(self.request, 'Absence request submitted successfully!')
 
         return redirect(reverse('home:home'))
