a39f994c8b492775b6a6ce7d3f013ae60edbecb4
Zilvinas Pocius <zbvc382@live.rhul.ac.uk>
2018-03-10 06:31:07 +0000

    More changes

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn wsgi --log-file -
+web: gunicorn mysite.wsgi --log-file -
--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -73,7 +73,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'wsgi.application'
+WSGI_APPLICATION = 'mysite.wsgi.application'
 
 
 # Database
